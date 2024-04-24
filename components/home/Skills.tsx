b1f4a161a251b0c277c585a48e0dab6a55f7ee6a
faizanjavair <faizanjavair@gmail.com>
2024-04-24 16:19:59 +0500

    added projects

--- components/home/Skills.tsx
+++ components/home/Skills.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { skills } from "@/data/content/home";
+import { skills, tools } from "@/data/content/home";
 import SectionTitle from "../global/SectionTitle";
 function Skills() {
   return (
     <div>
     <div id="learnmore" className="mb-10">
-        <SectionTitle title="Skills & Tools I have used." />
+        <SectionTitle title="Languages, Skills and Tools." />
     </div>
     <div className="flex flex-col md:flex-row justify-between relative mt-10 mb-30">
       
-        <div className="ml-4">
+        <div>
         <h2 className="relative text-2xl w-full text-center md:text-left font-bold md:max-w-lg mb-10 md:mr-10 md:mb-0 md:w-max mr-0">
         {""}
         <br />
-        Here are the tools I have used.
+        The <span className="text-fun-pink">Languages</span> and <span className="text-fun-pink">Frameworks</span> I have used.
         <img
           className="sqD bottom-[-80px] left-[-50px] lg:bottom-[-50px] lg:left-[-35px] z-[-10]"
           src="/static/doodles/skills/laptop.svg"
@@ -54,11 +54,11 @@ function Skills() {
     </div>
     <div className="flex flex-col md:flex-row justify-between relative lg:my-20 sm:mt-30">
       
-        <div className="ml-4">
+        <div>
         <h2 className="relative text-2xl w-full text-center md:text-left font-bold md:max-w-lg mb-10 md:mr-10 md:mb-0 md:w-max mr-0">
         {""}
         <br />
-        Here are the tools I have used.
+        The <span className="text-fun-pink">Tools</span> I have used to achieve <span className="text-fun-pink">success.</span>
         <img
           className="sqD bottom-[-80px] left-[-50px] lg:bottom-[-50px] lg:left-[-35px] z-[-10]"
           src="/static/doodles/skills/laptop.svg"
@@ -77,7 +77,7 @@ function Skills() {
       
       <div className="justify-end lg:mr-8 md:mr-8">
       <div className="relative max-w-lg w-full mx-auto md:mx-none grid gap-x-8 gap-y-12 sm:gap-8 md:gap-12 grid-cols-3 sm:grid-cols-6 items-center place-content-center">
-        {skills.map((item, index) => {
+        {tools.map((item, index) => {
           return (
             <div className="bg-fun-gray-dark w-20 p-4 rounded-lg">
             <div
--- data/content/home.ts
+++ data/content/home.ts
@@ -3,6 +3,11 @@ type Skill = {
   icon: string,
   style?: object
 };
+type Tool = {
+  title: string,
+  icon: string,
+  style?: object
+};
 type Testimonial = {
   quote: string,
   name: string,
@@ -16,53 +21,105 @@ export const skills: Skill[] = [
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
   },
   {
-    title: "CSS",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-  },
-  {
     title: "Javascript",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
   },
   {
-    title: "Sass",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+    title: "Python",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
   },
   {
-    title: "TailwindCSS",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    title: "C++",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+  },
+  {
+    title: "C#",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
   },
   {
     title: "React",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
   },
   {
-    title: "NextJS",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
-    style: { filter: "invert(1)" },
-  },
-  {
-    title: "Typescript",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    title: "Django",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
+    style: {filter: "invert(1)"}
   },
   {
     title: "NodeJS",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
   },
   {
-    title: "Git",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-  },
-  {
     title: "Firebase",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
   },
   {
-    title: "Figma",
-    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    title: "PostgreSQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+  },
+  {
+    title: "MySQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+  },
+  {
+    title: "Unity",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg",
   },
   
 ];
-
+export const tools: Tool[] = [
+  {
+    title: "Git",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+  },
+  {
+    title: "Github",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    style: {filter: "invert(1)"}
+  },
+  {
+    title: "GitLab",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg",
+  },
+  {
+    title: "CSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    title: "TailwindCSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+  },,
+  {
+    title: "Bootstrap",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+  },
+  {
+    title: "NGINX",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg",
+  },
+  {
+    title: "Jupyter",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg",
+  },
+  {
+    title: "Docker",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg",
+  },
+  {
+    title: "Linux",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+  },
+  ,
+  {
+    title: "Vagrant",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vagrant/vagrant-original.svg",
+  },
+  {
+    title: "CI/CD",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/githubactions/githubactions-original.svg",
+  }
+  
+];
 export const testimonials: Testimonial[] = [
   {
     quote:
